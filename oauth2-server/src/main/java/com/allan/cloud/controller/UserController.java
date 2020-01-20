4d4647240b8b9af1de7b69d6186cd3c8dc0e7bc4
yanglin1501804006 <yanglin02@raisecom.com>
2020-01-20 11:45:24 +0800

    add oauth2-server

---
+++ oauth2-server/src/main/java/com/allan/cloud/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.allan.cloud.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by allan on 2020/1/19.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
